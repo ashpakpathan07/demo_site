9b6c3b0f798c0243c4b32aefe1ab40d48435d715
ashpak pathan <ashpakpathan888@gmail.com>
2023-03-19 17:55:20 +0530

    chang

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-//import { Hero } from "./compo/Hero";
+import { Hero } from "./compo/Hero";
 import { Navbar } from "./compo/Navbar";
 // import { About } from "./compo/About";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+//import { BrowserRouter,Routes,Route } from "react-router-dom";
 import { About } from "./compo/About";
 import { Services } from "./compo/Services";
 
 function App() {
   return (
     <div className="App">
-
+{/* 
 <BrowserRouter>
       <Routes>
           <Route path="/"  element={<Navbar />} >
@@ -19,14 +19,12 @@ function App() {
           <Route path="Services"  element={<Services />} />
         </Route>
       </Routes>
-    </BrowserRouter>
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navbar />} />
-          <Route path="About-Us" element={<About />} />
-          <Route path="Services" element={<Services />} />
-        </Routes>
-      </BrowserRouter> */}
+    </BrowserRouter> */}
+    <Navbar/>
+    <Hero />
+    <About />
+    <Services />
+
     </div>
   );
 }
